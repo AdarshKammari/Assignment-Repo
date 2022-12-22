c66228bdd7d4dad96f11b6cd6b77fd4a329e40be
NAGARRO\adarshkammari <adarsh.kammari@nagarro.com>
2022-12-22 09:53:25 +0530

    first Commit

---
+++ src/gitClass.java
@@ -0,0 +1,40 @@
+
+
+public class gitClass {int x,y,a,b;
+private String name ="Encapsulation" ;
+public String Name="Inheritance";
+public void type() {
+	if (x == y) {
+		System.out.println("There is no information about this class's inhertance"+" "+ "This class is " +name);
+	}
+	else {
+		System.out.println("The Class got Inherited"+" "+Name);
+	}
+}
+public void newmethod() {
+	
+	if(a!=b) {
+		System.out.println("Message here");
+		
+	}
+}
+public void amethod() {
+	//this is also a method.//
+	//for committing//
+	System.out.println("message here");
+}
+
+public void mergemethod() {
+	//this is a method used for merging.//
+
+
+}
+
+
+public static void main(String[] args) {
+	gitClass obj = new gitClass();
+	obj.type();
+
+}
+
+}
